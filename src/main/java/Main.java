9dee3a879c3f96da34663548a8793f7509333666
Georgiy051188 <Geogiy88@gmail.com>
2019-06-25 19:22:22 +0300

    Start project. Extracture. Edd log4j.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
+import org.apache.log4j.Logger;
+
 public class Main {
+    private final  static Logger logger = Logger.getLogger(Main.class);
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        logger.debug("hello");
+
     }
 }
